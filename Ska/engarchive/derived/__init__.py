ed97d71365d96db2574672452cf9e3ac4166ab6b
Tom Aldcroft <taldcroft@gmail.com>
2011-09-08 18:19:01 -0400

    Refactor MD's derived equations into derived sub-package + thermal.py

---
+++ Ska/engarchive/derived/__init__.py
@@ -0,0 +1,3 @@
+from .base import DerivedParameter
+
+from .thermal import *
--- Ska/engarchive/derived/thermal.py
+++ Ska/engarchive/derived/thermal.py
@@ -1,24 +1,6 @@
-import Ska.engarchive.fetch_eng as fetch_eng
 import numpy as np
 
-
- 
-class DerivedParameter(object):
-    def calc(self,data):
-        raise NotImplementedError
-
-    def __call__(self, start, stop):
-        data = fetch_eng.MSIDset(self.rootparams, start, stop, filter_bad=True)
-        for name in self.rootparams:
-            if isinstance(data[name].vals[0],(str,unicode)):
-                if 'ON' in data[name].vals[0] or 'OFF' in data[name].vals[0]:
-                    data[name].vals = np.array([d.strip() == 'ON' for d in data[name].vals])
-                    
-        data.interpolate(dt=self.timestep)
-
-        return self.calc(data)
-
-    
+from .base import DerivedParameter
 
 #--------------------------------------------
 class DP_EE_AXIAL(DerivedParameter):
