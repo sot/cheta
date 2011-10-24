01fdda96ab81d9e0db98fc0c1195e44945d43e56
Tom Aldcroft <taldcroft@gmail.com>
2011-10-24 17:53:29 -0400

    Update version and add_derived NOTES

--- Ska/engarchive/version.py
+++ Ska/engarchive/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.12dev'
+version = '0.12'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
