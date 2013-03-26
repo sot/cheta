9fec1717440e51d4035970e003730d2fd0bb36e0
Tom Aldcroft <taldcroft@gmail.com>
2013-03-26 11:56:30 -0400

    Change version from 0.21 to 0.22

--- Ska/engarchive/version.py
+++ Ska/engarchive/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.21'
+version = '0.22'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
