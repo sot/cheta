6f4d978a42b5c38d7360811accf40602a4911238
Tom Aldcroft <taldcroft@gmail.com>
2014-03-05 22:47:12 -0500

    Update version to 0.28

--- Ska/engarchive/version.py
+++ Ska/engarchive/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.27'
+version = '0.28'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
