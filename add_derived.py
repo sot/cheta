9426c8c7b8fe681a6b8f5187b0b3bac71c62c49d
Tom Aldcroft <taldcroft@gmail.com>
2014-05-19 17:39:36 -0400

    Remove leftover debug print statement

--- add_derived.py
+++ add_derived.py
@@ -127,7 +127,6 @@ def main():
         colname = dp_class.__name__.upper()
         dp = dp_class()
         content = dp.content
-        print content, opt.content
         if not opt.content or (opt.content and content in opt.content):
             dpd = content_defs.setdefault(content, {})
             dpd.setdefault('classes', {'TIME': None})
