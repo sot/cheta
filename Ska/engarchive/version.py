611836c804b756e31ea15c24613bad1b6881bd53
Tom Aldcroft <taldcroft@gmail.com>
2014-09-03 16:01:14 -0400

    Update version to 0.35-dev

--- Ska/engarchive/version.py
+++ Ska/engarchive/version.py
@@ -33,7 +33,7 @@ import os
 ### SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (0, 34, 0, False)
+VERSION = (0, 35, None, True)
 
 
 class SemanticVersion(object):
