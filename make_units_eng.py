0c23d1d77e90633fbe565c4a25c3522a0f70d1ba
Tom Aldcroft <taldcroft@gmail.com>
2016-07-05 15:31:57 -0400

    Make_units_eng now copies all CXC units and defaults to degC for Kelvins

--- make_units_eng.py
+++ make_units_eng.py
@@ -3,17 +3,23 @@ Make a unit_system consistent with usual OCC/FOT engineering units via P009.
 """
 
 import pickle
+from copy import copy
 
 import Ska.engarchive.converters
 import Ska.tdb
 
-dat = Ska.tdb.tables['tmsrment']
-
 units_cxc = pickle.load(open('units_cxc.pkl'))
+units_eng = copy(units_cxc)
 
-units_eng = dict((msid.upper(), unit)
-                 for msid, unit in zip(dat['MSID'], dat['ENG_UNIT'])
-                 if unit and msid.upper() in units_cxc)
+# Update with units from TMSRMENT table of the TDB
+dat = Ska.tdb.tables['tmsrment']
+tdb_units = {msid.upper(): unit for msid, unit in zip(dat['MSID'], dat['ENG_UNIT'])}
+units_eng.update({msid: tdb_units[msid] for msid, unit in units_eng.items()
+                  if msid in tdb_units})
+
+# Any other MSIDs that still have units of 'K' are converted to DEGC
+units_eng.update({msid: 'DEGC' for msid, unit in units_eng.items()
+                  if unit in ['K', 'deltaK']})
 
 # Use info about DEA HK telemetry from converters to add units
 for col in Ska.engarchive.converters._get_deahk_cols():
