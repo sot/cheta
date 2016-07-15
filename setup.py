f072eab09838cb2e67237b3f69d346a5ad06cbbc
Tom Aldcroft <taldcroft@gmail.com>
2016-07-15 15:30:08 -0400

    Add stuff for python setup.py test

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 from Ska.engarchive.version import package_version
+
+
+class PyTest(TestCommand):
+    user_options = [('args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.args = []
+
+    def run_tests(self):
+        # Import here because outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.args)
+        sys.exit(errno)
+
 
 # Write GIT revisions and SHA tag into <this_package/git_version.py>
 # (same directory as version.py)
@@ -20,4 +37,6 @@ setup(name='Ska.engarchive',
       package_dir={'Ska': 'Ska'},
       package_data={'Ska.engarchive': ['*.dat', 'units_*.pkl', 'GIT_VERSION'],
                     'Ska.engarchive.tests': ['*.dat']},
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
       )
