1b8fbb19651f272a5a83752889a6a3e0a4ecc708
Tom Aldcroft <taldcroft@gmail.com>
2019-07-11 20:22:45 -0400

    New approach using package_dir directive

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import shutil
-import sys
-import os
 from setuptools import setup
 
 try:
@@ -9,36 +6,22 @@ try:
 except ImportError:
     cmdclass = {}
 
-# On Windows the `cheta` soft link can turn into a plain file, so work
-# around this by temporarily moving that file to the side and doing a
-# full tree copy of Ska/engarchive => cheta.
-replace_cheta = sys.platform.startswith('win') and os.path.isfile('cheta')
+setup(name='Ska.engarchive',
+      author='Tom Aldcroft',
+      description='Modules supporting Ska engineering telemetry archive',
+      author_email='aldcroft@head.cfa.harvard.edu',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      entry_points={'console_scripts': ['ska_fetch = Ska.engarchive.get_telem:main']},
+      zip_safe=False,
+      package_dir={'Ska': 'Ska', 'cheta': 'Ska/engarchive'},
+      packages=['Ska', 'Ska.engarchive', 'Ska.engarchive.derived', 'Ska.engarchive.tests',
+                'cheta', 'cheta.derived', 'cheta.tests'],
+      package_data={'Ska.engarchive': ['*.dat', 'units_*.pkl'],
+                    'Ska.engarchive.tests': ['*.dat'],
+                    'cheta': ['*.dat', 'units_*.pkl'],
+                    'cheta.tests': ['*.dat']},
 
-try:
-    if replace_cheta:
-        os.rename('cheta', 'cheta-TEMPORARY')
-        shutil.copytree(os.path.join('Ska', 'engarchive'), 'cheta')
-
-    setup(name='Ska.engarchive',
-          author='Tom Aldcroft',
-          description='Modules supporting Ska engineering telemetry archive',
-          author_email='aldcroft@head.cfa.harvard.edu',
-          use_scm_version=True,
-          setup_requires=['setuptools_scm'],
-          entry_points={'console_scripts': ['ska_fetch = Ska.engarchive.get_telem:main']},
-          zip_safe=False,
-          packages=['Ska', 'Ska.engarchive', 'Ska.engarchive.derived', 'Ska.engarchive.tests',
-                    'cheta', 'cheta.derived', 'cheta.tests'],
-          package_data={'Ska.engarchive': ['*.dat', 'units_*.pkl'],
-                        'Ska.engarchive.tests': ['*.dat'],
-                        'cheta': ['*.dat', 'units_*.pkl'],
-                        'cheta.tests': ['*.dat']},
-
-          tests_require=['pytest'],
-          cmdclass=cmdclass,
-          )
-
-finally:
-    if replace_cheta:
-        shutil.rmtree('cheta')
-        os.rename('cheta-TEMPORARY', 'cheta')
+      tests_require=['pytest'],
+      cmdclass=cmdclass,
+      )
