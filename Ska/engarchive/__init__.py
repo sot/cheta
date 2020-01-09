a3d1c32ce1ad2fc46c116ad8be9dd5193af28238
Javier Gonzalez <javierggt@yahoo.com>
2020-01-09 15:24:38 -0500

    Revert "Use __package__ also in cases where module is not defined in __init__.py"
    This reverts commit 8d5dc5704347d94a73a0b8eb8d46882629d2fa67.

--- Ska/engarchive/__init__.py
+++ Ska/engarchive/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import ska_helpers
-__version__ = ska_helpers.get_version(__package__)
+__version__ = ska_helpers.get_version('Ska.engarchive')
 
 
 def test(*args, **kwargs):
