94b5fa3874b2fb1893daa38655c5fd710b84efe4
Tom Aldcroft <taldcroft@gmail.com>
2026-06-29 13:54:36 -0400

    Define MSID.__len__ using self.times instead of self.vals
    self.vals might not exist, e.g. for stats on a 2-d MSID.

--- cheta/fetch.py
+++ cheta/fetch.py
@@ -617,7 +617,7 @@ class MSID(object):
         return content.get(self.MSID) if "cxc" in data_source.sources() else None
 
     def __len__(self):
-        return len(self.vals)
+        return len(self.times)
 
     @property
     def dtype(self) -> np.dtype:
